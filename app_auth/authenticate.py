5b63758ddb41eb26c890789f4aebf8981a3a5948
m-schaar@gmx.net <m-schaar@gmx.net>
2026-02-07 17:20:05 +0100

    .

--- app_auth/authenticate.py
+++ app_auth/authenticate.py
@@ -3,12 +3,10 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 
 class CookieJWTAuthentication(JWTAuthentication):
     def authenticate(self, request):
-        # Wir extrahieren den Token aus dem Cookie statt aus dem Header
         raw_token = request.COOKIES.get('access_token') or None
 
         if raw_token is None:
             return None
 
-        # Der Rest wird von der Basis-Klasse erledigt
         validated_token = self.get_validated_token(raw_token)
         return self.get_user(validated_token), validated_token
